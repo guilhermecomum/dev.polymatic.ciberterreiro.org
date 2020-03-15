81db01e2c0ee3c69880727bbffd8edd6bf259788
Guilherme Guerra <guilherme.ga@gmail.com>
2020-03-15 18:03:31 -0300

    Updates

---
+++ precache-manifest.544dd27dbb4c3b1817f38a33f6f9dc9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71938d10f3e3abb800f0bd5bff09a54f",
+    "url": "/polyclave/index.html"
+  },
+  {
+    "revision": "72c9366ea418c2a944d1",
+    "url": "/polyclave/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1b99a6bb95164f542692",
+    "url": "/polyclave/static/css/main.09416a13.chunk.css"
+  },
+  {
+    "revision": "72c9366ea418c2a944d1",
+    "url": "/polyclave/static/js/2.7a0323b6.chunk.js"
+  },
+  {
+    "revision": "65cb5994b1fcbc6a1abeaeba28184b3b",
+    "url": "/polyclave/static/js/2.7a0323b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b99a6bb95164f542692",
+    "url": "/polyclave/static/js/main.a563e73e.chunk.js"
+  },
+  {
+    "revision": "99e2bd30a4d0515c3e63",
+    "url": "/polyclave/static/js/runtime-main.29dd66d4.js"
+  },
+  {
+    "revision": "f3e648cbe6fa1f039ecd00253f568260",
+    "url": "/polyclave/static/media/hit2.f3e648cb.wav"
+  }
+]);
